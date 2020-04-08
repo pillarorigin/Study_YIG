b5b0110113047b7c42f8f9c193511f3460010a37
pillarorigin <pillar_origin@naver.com>
2020-04-09 02:04:47 +0900

    java basic(+unicode tip)

---
+++ 5. Compiled Language/5. Java/Day02/src/com/test/MTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class MTest {
+	
+			//public static final PrintStream out
+	public static void main(String[] args) {
+
+		System.out.println();
+		System.out.println(true); //boolean
+		System.out.println(100+100);//정수:short int(default) long
+		System.out.println(97.9); //실수: float double(default)
+		System.out.println('a'); //한문자 char
+		System.out.println("abc"); //문자열 string
+		System.out.println("a="+100+100); //+ ㅣ 연결 문자열 연산자
+		
+       }
+	
+ 	}
